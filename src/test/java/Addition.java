bc15fdad9b94f64810ab08043b41d10c29306abb
abinash <dj.avinash55@gmail.com>
2023-01-08 21:36:36 +0530

    Second commit addition

---
+++ src/test/java/Addition.java
@@ -0,0 +1,14 @@
+public class Addition
+{
+    public static void main(String args[])
+    {
+        int a=8;
+        int b=9;
+
+        int c= a=b;
+
+        System.out.println(c);
+
+    }
+
+}
